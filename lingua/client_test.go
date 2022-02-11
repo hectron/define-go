4a116e266b9ca0b80e8dbb2c2828cd5ac1b63e6e
Hector Rios <that.hector@gmail.com>
2022-02-11 12:43:44 -0600

    Wrap up tests for defining words

--- lingua/client_test.go
+++ lingua/client_test.go
@@ -1,8 +1,11 @@
 package lingua
 
 import (
+	"bytes"
 	"errors"
+	"io/ioutil"
 	"net/http"
+	"reflect"
 	"testing"
 )
 
@@ -51,5 +54,69 @@ func TestDefine(t *testing.T) {
 		linguaClient := Lingua{HttpClient: &MockClient{}, ApiKey: "test-key"}
 
 		linguaClient.Define(word)
+	})
+
+	t.Run("It returns a valid summary", func(t *testing.T) {
+		MockedDoFunction = func(req *http.Request) (*http.Response, error) {
+			return &http.Response{
+				StatusCode: 200,
+				Body: ioutil.NopCloser(
+					bytes.NewReader(
+						[]byte(`{
+			"entries": [
+				{
+					"entry": "jejune",
+					"lexemes": [
+						{
+							"partOfSpeech": "adjective",
+							"senses": [
+								{
+									"definition": ""
+								}
+							]
+						}
+					],
+					"pronunciations": [
+						{
+							"context": {
+								"regions": [
+									"United States"
+								]
+							},
+							"transcriptions": [
+								{
+									"notation": "IPA",
+									"transcription": "/jay-june/"
+								}
+							]
+						}
+					]
+				}
+			]
+		}`))),
+			}, nil
+		}
+
+		client := Lingua{HttpClient: &MockClient{}}
+
+		want := Summary{
+			Word:          "jejune",
+			Pronunciation: "/jay-june/",
+			Definitions: []Definition{
+				{
+					Meaning:      "",
+					PartOfSpeech: "adjective",
+				},
+			},
+		}
+		got, err := client.Define("jejune")
+
+		if err != nil {
+			t.Errorf("Did not expect error, but got %v", err)
+		}
+
+		if !reflect.DeepEqual(got, want) {
+			t.Errorf("got %q, want %q", got, want)
+		}
 	})
 }
